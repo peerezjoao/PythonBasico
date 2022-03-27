e6609375f5bf00f61680690b2a391ee2a9a0e815
João Perez <joao.perez@digitaly.tech>
2022-03-27 18:01:04 -0300

    entendendo sobre o print()

---
+++ aula2.py
@@ -0,0 +1,11 @@
+# mais sobre o PRINT()
+
+# print(1234)
+print("João", "Vitor", "Perez")
+print("João", "Vitor", "Perez", sep="-")
+print("João", "Vitor", "Perez", sep="/", end='')
+print("João", "Vitor", "Perez", sep="-")
+
+# formatando o CPF com a mascara
+print("824", "176", "070", sep=".", end="-")
+print(18)
