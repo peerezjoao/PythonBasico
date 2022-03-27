7323c3ffe98edeb6b1fadd9c64a3a2755e5a23c0
João Perez <joao.perez@digitaly.tech>
2022-03-27 18:04:04 -0300

    operadores matemáticos

---
+++ aula5.py
@@ -0,0 +1,24 @@
+"""
+Operadores matemáticos
+
++ - / *
+// - divisão inteira
+** exponeciacao
+% resto da divisão
+() alterar a precedencia
+"""
+
+print("Multiplicação: ", 10 * 10)
+print("Adição: ", 10 + 10)
+print("Subtração: ", 10 - 1)
+print("Divisão : ", 10 / 2)
+
+print("*----------Casos exclusivos-----------------*")
+print("Multiplicação: ", 10 * 'João')
+print("Multiplicação: ", "10" + "10")
+
+print("*----------------------*")
+print(10//3)
+print(11**3)
+print(10%3)
+print((5+2)*10)
