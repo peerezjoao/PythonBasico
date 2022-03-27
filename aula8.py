3c7242208bf4875f81f1bf3c5f23e27394061c6f
João Perez <joao.perez@digitaly.tech>
2022-03-27 18:05:56 -0300

    entrada de dados

---
+++ aula8.py
@@ -0,0 +1,16 @@
+"""
+Entrada de dados
+"""
+
+# nome = input("Qual seu nome? ")
+# idade = input("Qual sua idade? ")
+# ano_nascimento = 2021 - int(idade)
+#
+#
+# print(f'{nome} tem {idade} anos')
+# print(f'Ano de nascimento: {ano_nascimento}')
+
+numero_1 = int(input("Digite um número: "))
+numero_2 = int(input("Digite outro número: "))
+
+print(numero_1 ** numero_2)
