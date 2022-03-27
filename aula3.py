6577f890f6f44cadebe5f3c93fae7958edb961cd
João Perez <joao.perez@digitaly.tech>
2022-03-27 18:02:41 -0300

    strings

---
+++ aula3.py
@@ -0,0 +1,6 @@
+"""
+str - String
+"""
+print("Essa é uma 'string' (str).")
+print('Essa é uma "string" (str).')
+print("Essa é uma \"string\" (str).") # caractere de escape - deixa o código poluido
