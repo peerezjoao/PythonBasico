73c620dd49ed3e215d4c03f15f7d3183d1dc67db
João Perez <joao.perez@digitaly.tech>
2022-03-27 18:08:37 -0300

    manipulando strings

---
+++ aula15.py
@@ -0,0 +1,20 @@
+"""
+Manipulando Strings
+"""
+
+# positivos
+#       [012456789]
+texto = 'Python_s2'
+# negativos
+#      -[987654321]
+
+# url = 'www.google.com.br/'
+# print(url[:-1])
+
+# Fatiamentos
+
+nova_string = texto[0:6]
+print(nova_string)
+
+nova_string = texto[::2]
+print(nova_string)
