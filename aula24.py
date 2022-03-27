12940957f8e84241023f01025524f167ecae1a4c
João Perez <joao.perez@digitaly.tech>
2022-03-27 18:11:36 -0300

    variável auxiliar

---
+++ aula24.py
@@ -0,0 +1,17 @@
+"""
+Invertendo variáveis
+"""
+
+x = 10
+y = 'joao'
+z = 'marco'
+
+# z = x
+# x = y             utilizando uma variável auxiliar
+# y = z
+x, y = y, x   # forma simplificada de inverter os valores
+
+x, y, z = z, x, y
+
+
+
