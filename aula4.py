2c1613569c691486815240e8bd74aa568a0715b6
João Perez <joao.perez@digitaly.tech>
2022-03-27 18:03:42 -0300

    tipos de dados

---
+++ aula4.py
@@ -0,0 +1,29 @@
+"""
+Tipos de dados
+
+str - String - textos 'assim' "assim"
+int - inteiro - 123456789  10  25 15400 -54
+float - real/ponto flutuante - 120.0  1254.00 -540.0 10.10
+bool - booleano/lógico -  True False
+"""
+# str
+print('João', type('João'))
+# int
+print(123, type(123))
+# float
+print(147.0, type(147.0))
+# bool
+print(10 > 45, type(10 > 45))
+
+
+print('João', type('João'), bool('João'))
+print(15, type(15), bool(15))
+print('10', type('10'), type(int('10')))
+
+print("*------------------*")
+
+print("João", type("João"))
+print(21, type(21))
+print(1.65, type(1.65))
+print(21 >= 18, type(21 >= 18))
+
